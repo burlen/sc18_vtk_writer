5757eb3aa8e89a3de9bd80efa5f1870001f32e04
Burlen Loring <bloring@lbl.gov>
2018-11-08 14:29:46 -0800

    added cori demo

--- oscillator_posthoc_paraview.sh
+++ oscillator_posthoc_paraview.sh
@@ -4,9 +4,20 @@ n=4
 b=64
 dt=2
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+mkdir -p ./configs
+cp ${script_dir}/configs/* ./configs
+
+echo "+ module use /usr/common/software/sensei/modulefiles"
+module use /usr/common/software/sensei/modulefiles
+
 echo "+ module load sensei/2.1.1-vtk-shared"
-module load sensei/2.1.1-vtk-shared
+module load sensei/2.1.0-vtk-shared
 
 set -x
-mpiexec -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -g 1 -p 0 \
+
+cat ./configs/write_pvd.xml
+
+srun -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -p 0 \
     -f ./configs/write_pvd.xml ./configs/random_2d_${b}.osc
--- oscillator_posthoc_visit.sh
+++ oscillator_posthoc_visit.sh
@@ -4,9 +4,21 @@ n=4
 b=64
 dt=2
 
-echo "+ module load sensei/2.1.1-libsim-shared"
-module load sensei/2.1.1-libsim-shared
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+mkdir -p ./configs
+cp ${script_dir}/configs/* ./configs
+
+echo "+ module use /usr/common/software/sensei/modulefiles"
+module use /usr/common/software/sensei/modulefiles
+
+echo "+ module load sensei/2.1.0-vtk-shared"
+module load sensei/2.1.0-vtk-shared
 
 set -x
-mpiexec -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -g 1 -p 0 \
-    -f ./configs/write_visit.xml ./configs/random_2d_${b}.osc
+
+cat ./configs/write_visit.xml
+
+srun -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -p 0 \
+    -f ./configs/write_visit.xml \
+    ./configs/random_2d_${b}.osc
