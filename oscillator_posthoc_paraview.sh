53a3ce2e3f76aa0a0bf52f771faa01ca53accf70
sensei user <sensei-sc19@dev.null>
2019-11-14 15:43:13 -0800

    updates for sc19

--- oscillator_posthoc_paraview.sh
+++ oscillator_posthoc_paraview.sh
@@ -4,8 +4,8 @@ n=4
 b=64
 dt=2
 
-echo "+ module load sensei/2.1.1-vtk-shared"
-module load sensei/2.1.1-vtk-shared
+echo "+ module load sensei/3.0.0-vtk-shared"
+module load sensei/3.0.0-vtk-shared
 
 set -x
 mpiexec -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -g 1 -p 0 \
